33a29a0b1cbf754a52f81b8bee090c324c98a1c2
Matheus Lopes Trindade <matheus.lopes.trindade@hotmail.com>
2023-08-22 00:07:43 -0300

    adding Login page

---
+++ src/app/login/page.jsx
@@ -0,0 +1,78 @@
+'use client'
+
+// icons
+import { BsArrowRight } from 'react-icons/bs';
+
+// framer motion
+import { motion } from 'framer-motion';
+
+// variants
+import { fadeIn } from '../../../variants';
+
+export const metadata = {
+  title: 'Login | PI4',
+  description: 'Login PI4',
+  OpenGraph: {
+    title: 'Login | PI4',
+    description: 'Login PI4.', 
+  }
+}
+
+export default function Login(params) {
+  return (
+    <main className='h-[100vh] bg-primary/30'>
+      <div
+        className='
+          container mx-auto py-32 text-center xl:text-left 
+          flex items-center justify-center h-full'
+        >
+        {/* text & form */}
+        <div className='flex flex-col w-full max-w-[700px]'>
+          {/* text */}
+          <motion.h1 
+            variants={ fadeIn('up', 0.2) }
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+            className='h2 text-center mb-12 max-sm:text-[30px]'
+          >
+            Vamos <span className='text-accent'>logar.</span>
+          </motion.h1>
+          {/* form */}
+          <motion.form
+            variants={ fadeIn('up', 0.4) }
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+            action=''
+            className='flex-1 flex flex-col items-center justify-center gap-6 w-full mx-auto'
+          >
+            {/* input group */}
+            <div className='flex flex-col gap-6 w-[300px]'>
+              <input type='text' name='username' placeholder='username' className='input'/>
+              <input type='password' name='password' placeholder='password' className='input'/>
+            </div>
+            <button className='
+              btn rounded-full border border-white/50 max-w-[120px] px-8 
+              transition-all delay-300 flex items-center justify-center 
+              overflow-hidden hover:border-accent hover:border-2 group'
+            >
+              <span className='
+                group-hover:-translate-y-[120%] group-hover:opacity-0 
+                transition-all delay-200 duration-200'
+              >
+                Logar
+              </span>
+              <BsArrowRight className='
+                -translate-y-[120%] opacity-0 group-hover:flex 
+                group-hover:-translate-y-0 group-hover:opacity-100 
+                transition-all delay-200 duration-200 absolute text-[30px] 
+              text-accent font-bold' 
+              />
+            </button>
+          </motion.form>
+        </div>
+      </div>
+    </main>
+  )
+};
