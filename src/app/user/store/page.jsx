9226b90f0481ac572a50081286e9f724d1753b52
Matheus Lopes Trindade <matheus.lopes.trindade@hotmail.com>
2023-09-05 22:16:11 -0300

    create store page

---
+++ src/app/user/store/page.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+	title: 'Troca Facil | Products',
+	description: 'Products',
+};
+
+export default function Products(params) {
+  return (
+    <main>
+      <h1 className='h1'>Products</h1>
+    </main>
+  )
+};
