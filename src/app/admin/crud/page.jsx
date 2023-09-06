c580b21b2bdb02d7a59065c505a5e96f9a01ed74
Matheus Lopes Trindade <matheus.lopes.trindade@hotmail.com>
2023-09-05 22:14:20 -0300

    create crud admin page

---
+++ src/app/admin/crud/page.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+	title: 'Troca Facil | Admin - CRUD',
+	description: 'CRUD Admin',
+};
+
+export default function CRUD(params) {
+  return (
+    <main>
+      <h1 className='h1'>CRUD</h1>
+    </main>
+  )
+};
