afc019072996eb3747891436762cdb44e5907f5b
Matheus Lopes Trindade <matheus.lopes.trindade@hotmail.com>
2023-09-05 22:01:16 -0300

    adding navData (admin and user)

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,13 +8,38 @@ import {
 	HiOutlineInformationCircle,
 } from 'react-icons/hi2';
 
+import {
+	BiSolidDashboard,
+	BiSolidData,
+} from 'react-icons/bi'
+
+import {
+	SiHomeassistantcommunitystore
+} from 'react-icons/si'
+import {
+	GiExitDoor
+} from 'react-icons/gi'
+
 // nav data
-export const navData = [
-	{ name: 'home', path: '/', icon: <HiHome /> },
-	{ name: 'login', path: '/login', icon: <HiUser /> },
-	{ name: 'register', path: '/register', icon: <HiPencilSquare /> },
-	{ name: 'info', path: '/info', icon: <HiOutlineInformationCircle /> }
-];
+export const navData = {
+	comumn: [
+		{ name: 'home', path: '/', icon: <HiHome /> },
+		{ name: 'login', path: '/login', icon: <HiUser /> },
+		{ name: 'register', path: '/register', icon: <HiPencilSquare /> },
+		{ name: 'info', path: '/info', icon: <HiOutlineInformationCircle /> }
+	],
+	admin: [
+		{ name: 'dashboard', path: '/admin', icon: <BiSolidDashboard /> },
+		{ name: 'crud', path: '/admin/crud', icon: <BiSolidData /> },
+		{ name: 'logout', path: '/', icon: <GiExitDoor /> }
+	],
+	user: [
+		{ name: 'dashboard', path: '/user', icon: <BiSolidDashboard /> },
+		{ name: 'profile', path: '/user/profile', icon: <HiUser /> },
+		{ name: 'store', path: 'user/store', icon: <SiHomeassistantcommunitystore /> },
+		{ name: 'logout', path: '/', icon: <GiExitDoor /> }
+	]
+};
 
 // next
 import Link from 'next/link';
@@ -23,6 +48,8 @@ import { usePathname } from 'next/navigation';
 export default function Nav(params) {
 	const pathname = usePathname();
 
+	const navArray = pathname.startsWith('/admin') ? navData.admin : (pathname.startsWith('/user') ? navData.user : navData.comumn);
+
 	return (
 		<nav 
 			className='flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen'
@@ -30,7 +57,8 @@ export default function Nav(params) {
 			<div 
 				className='flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-14 md:px-40 xl:px-0 h-[80px] xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full shadow-lg drop-shadow-lg'
 			>
-				{navData.map((link, index) => {
+				
+				{navArray.map((link, index) => {
 					return (
 						<Link
 							name={link.name}
