cc162a6824ad8659a464ca7755fa3dec3ac0e796
Matheus Lopes Trindade <matheus.lopes.trindade@hotmail.com>
2023-09-05 22:14:47 -0300

    create User page

---
+++ src/app/user/page.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+	title: 'Troca Facil | User',
+	description: 'Dashboard User',
+};
+
+export default function User(params) {
+  return (
+    <main>
+      <h1 className='h1'>Dashboard User</h1>
+    </main>
+  )
+};
