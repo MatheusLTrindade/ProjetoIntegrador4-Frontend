583534d290f841cdd7da1a7d2fae7f07fa76e843
Matheus Lopes Trindade <matheus.lopes.trindade@hotmail.com>
2023-11-10 23:47:00 -0300

    separating json from Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,54 +1,36 @@
 'use client'
 
 // icons
-import {
-	HiHome,
-	HiUser,
-	HiPencilSquare,
-	HiOutlineInformationCircle,
-} from 'react-icons/hi2';
-
-import {
-	BiSolidDashboard,
-	BiSolidData,
-} from 'react-icons/bi'
-
-import {
-	SiHomeassistantcommunitystore
-} from 'react-icons/si'
-import {
-	GiExitDoor
-} from 'react-icons/gi'
+import { HiHome, HiUser, HiPencilSquare, HiOutlineInformationCircle } from 'react-icons/hi2';
+import { BiSolidDashboard, BiSolidData } from 'react-icons/bi'
+import { SiHomeassistantcommunitystore } from 'react-icons/si'
+import { GiExitDoor } from 'react-icons/gi'
 
 // next
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 // api/auth
-import authLogout from '@/app/api/auth/authLogout';
+import authLogout from '@/api/auth/authLogout';
 
 // nav data
-export const navData = {
-	comumn: [
-		{ name: 'login', path: '/login', icon: <HiUser /> },
-		{ name: 'info', path: '/info', icon: <HiOutlineInformationCircle /> }
-	],
-	admin: [
-		{ name: 'dashboard', path: '/admin', icon: <BiSolidDashboard /> },
-		{ name: 'crud', path: '/admin/crud', icon: <BiSolidData /> },
-		{ name: 'logout', path: '/', icon: <GiExitDoor /> }
-	],
-	user: [
-		{ name: 'dashboard', path: '/user', icon: <BiSolidDashboard /> },
-		{ name: 'profile', path: '/user/profile', icon: <HiUser /> },
-		{ name: 'store', path: 'user/store', icon: <SiHomeassistantcommunitystore /> },
-		{ name: 'logout', path: '/', icon: <GiExitDoor /> }
-	]
-};
-
+import navData from '@/data/navData.json';
 
 export default function Nav(params) {
 	const pathname = usePathname();
+
+	function renderIcon(iconName) {
+		const icons = {
+			HiUser,
+			HiOutlineInformationCircle,
+			BiSolidDashboard,
+			BiSolidData,
+			GiExitDoor,
+			SiHomeassistantcommunitystore
+		}
+		const IconComponent = icons[iconName];
+		return IconComponent && <IconComponent />;
+	}
 
 	const navArray = pathname.startsWith('/admin') ? navData.admin : (pathname.startsWith('/user') ? navData.user : navData.comumn);
 
@@ -59,7 +41,6 @@ export default function Nav(params) {
 			<div 
 				className='flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-14 md:px-40 xl:px-0 h-[80px] xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full shadow-lg drop-shadow-lg'
 			>
-				
 				{navArray.map((link, index) => {
 					return (
 						<Link
@@ -87,7 +68,7 @@ export default function Nav(params) {
 								</div>
 							</div>
 							{/* icon */}
-							<div className='text-2xl' name={link.name}>{link.icon}</div>
+							<div className='text-2xl' name={link.name}>{renderIcon(link.icon)}</div>
 						</Link>
 					);
 				})}
