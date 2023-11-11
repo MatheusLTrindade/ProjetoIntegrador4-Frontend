0cc111582ba05cb04cf9d8c5d85de40596a47561
Matheus Lopes Trindade <matheus.lopes.trindade@hotmail.com>
2023-11-11 14:57:13 -0300

    path correction

--- src/app/user/store/page.jsx
+++ src/app/user/store/page.jsx
@@ -1,14 +1,14 @@
 export const metadata = {
-	title: 'Troca Facil | Products',
-	description: 'Products',
+	title: 'Troca Facil | Store',
+	description: 'Store',
 };
 
-export default function Products(params) {
+export default function Store(params) {
   return (
     <main className='h-[100vh] bg-secondary/20'>
 			<div className='w-full h-full'>
         <div className='text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto'>
-          <h1 className='h1'>Products</h1>
+          <h1 className='h1'>Store</h1>
         </div>
       </div>
     </main>
