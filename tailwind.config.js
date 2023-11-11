2ee32dedd747ba9833b7e5da9e9b4728ddb95939
Matheus Lopes Trindade <matheus.lopes.trindade@hotmail.com>
2023-11-11 14:54:54 -0300

    content settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/api/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/data/**/*.json',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
