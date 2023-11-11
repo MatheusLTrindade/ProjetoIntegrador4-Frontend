22629e7aad1f5f67697a1eb2da598573cae925a3
Matheus Lopes Trindade <matheus.lopes.trindade@hotmail.com>
2023-11-10 23:49:55 -0300

    updating Store

--- src/app/user/store/page.jsx
+++ src/app/user/store/page.jsx
@@ -5,8 +5,12 @@ export const metadata = {
 
 export default function Products(params) {
   return (
-    <main>
-      <h1 className='h1'>Products</h1>
+    <main className='h-[100vh] bg-secondary/20'>
+			<div className='w-full h-full'>
+        <div className='text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto'>
+          <h1 className='h1'>Products</h1>
+        </div>
+      </div>
     </main>
   )
 };
