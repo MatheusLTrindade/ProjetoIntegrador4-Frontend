fbfb1aaee755d101ec32b84ed446e59cdc0c505b
Matheus Lopes Trindade <matheus.lopes.trindade@hotmail.com>
2023-11-10 23:43:24 -0300

    adding isAuthenticated api

---
+++ src/app/api/auth/isAuthenticated.js
@@ -0,0 +1,25 @@
+// jwt
+import { jwtDecode } from "jwt-decode";
+
+export default function isAuthenticated() {
+  const jwtToken = sessionStorage.getItem('jwtToken')
+  if (!jwtToken) {
+    // Token não encontrado, o usuário não está autenticado
+    return null
+  }
+  try {
+    // Decodificar o token JWT para obter as informações
+    const decoded = jwtDecode(jwtToken)
+    // // Verificar se há uma propriedade 'role' no token
+    if (decoded && decoded.role) {
+      // Retornar o tipo de usuário (user ou admin)
+      return decoded.role
+    } else {
+      // A propriedade 'role' não foi encontrada no token
+      return null
+    }
+  } catch (error) {
+    console.error('Erro ao decodificar o token:', error)
+    return null
+  }
+}
