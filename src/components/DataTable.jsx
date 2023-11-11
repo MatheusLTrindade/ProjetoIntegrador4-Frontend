481be6c937044f0fa1d94b49d1f19613708fd3a4
Matheus Lopes Trindade <matheus.lopes.trindade@hotmail.com>
2023-11-10 23:51:03 -0300

    updating DataTable

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -1,43 +1,42 @@
 'use client'
 
+// react
 import React, { useState, useMemo } from 'react';
-import data from '@/app/data/data.json';
 
-import { TbArrowBigRight, TbArrowBigLeft, TbDatabaseEdit, TbDatabaseMinus } from 'react-icons/tb'
+// icons
+import { TbArrowBigRight, TbArrowBigLeft, TbDatabaseEdit, TbDatabaseMinus } from 'react-icons/tb';
 
-const itemsPerPage = window.matchMedia('not all and (min-width: 640px)').matches ? 3 : 5
-
-export default function DataTable() {
-  const [currentPage, setCurrentPage] = useState(1);
-  const [searchTerm, setSearchTerm] = useState('');
+export default function DataTable({ data }) {
+  const [currentPage, setCurrentPage] = useState(1)
+  const [searchTerm, setSearchTerm] = useState('')
   const filteredData = useMemo(() => {
     return data.filter((item) =>
-      item.name.toLowerCase().includes(searchTerm.toLowerCase())
+    item.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
-  }, [searchTerm]);
-
-  const totalPages = Math.ceil(filteredData.length / itemsPerPage);
-  const startIndex = (currentPage - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
-  const currentItems = filteredData.slice(startIndex, endIndex);
-
+  }, [searchTerm])
+  const itemsPerPage = window.matchMedia('not all and (min-width: 640px)').matches ? 3 : 5
+  const totalPages = Math.ceil(filteredData.length / itemsPerPage)
+  const startIndex = (currentPage - 1) * itemsPerPage
+  const endIndex = startIndex + itemsPerPage
+  const currentItems = filteredData.slice(startIndex, endIndex)
   function handlePageChange(page) {
-    setCurrentPage(page);
+    setCurrentPage(page)
   };
   const visiblePageNumbers = []
   if (totalPages <= 3) {
     for (let i = 1; i <= totalPages; i++) {
-      visiblePageNumbers.push(i);
+      visiblePageNumbers.push(i)
     }
   } else {
     if (currentPage <= 2) {
-      visiblePageNumbers.push(1, 2, 3);
+      visiblePageNumbers.push(1, 2, 3)
     } else if (currentPage >= totalPages - 1) {
-      visiblePageNumbers.push(totalPages - 2, totalPages - 1, totalPages);
+      visiblePageNumbers.push(totalPages - 2, totalPages - 1, totalPages)
     } else {
-      visiblePageNumbers.push(currentPage - 1, currentPage, currentPage + 1);
+      visiblePageNumbers.push(currentPage - 1, currentPage, currentPage + 1)
     }
   }
+  const tableHeaders = Object.keys(data[0]); // Assuming all objects have the same structure
 
   return (
     <div className="container mx-auto mt-8">
@@ -49,48 +48,32 @@ export default function DataTable() {
         className="w-full p-2 mb-4 border rounded text-black/40 placeholder:font-normal font-semibold"
       />
       <div className="w-full overflow-x-auto">
-        <table className="w-full border-collapse">
-          <thead className='bg-blueLight uppercase text-sm'>
-            <tr>
-              <th className="border p-2 sticky left-0 z-[1] bg-blueLight">ID</th>
-              <th className="border p-2">Username</th>
-              <th className="border p-2">Name</th>
-              <th className="border p-2">CPF</th>
-              <th className="border p-2">Birthday</th>
-              <th className="border p-2">Phone</th>
-              <th className="border p-2">Email</th>
-              <th className="border p-2">Password</th>
-              <th className="border p-2">Zip</th>
-              <th className="border p-2">Address</th>
-              <th className="border p-2">District</th>
-              <th className="border p-2">Complement</th>
-              <th className="border p-2">Number</th>
-              <th className="border p-2"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {currentItems.map((item, index) => (
-              <tr key={item.id} className={index % 2 === 0 ? '' : 'bg-accent/10'}>
-                <td className={`border p-2 sticky left-0 z-[1] ${index % 2 === 0 ? 'bg-[#C3C6CA]' : 'bg-[#AFB7BF]'}`}>{item.id}</td>
-                <td className="border p-2">{item.name}</td>
-                <td className="border p-2">{item.category}</td>
-                <td className="border p-2">{item.id}</td>
-                <td className="border p-2">{item.id}</td>
-                <td className="border p-2">{item.id}</td>
-                <td className="border p-2">{item.id}</td>
-                <td className="border p-2">{item.id}</td>
-                <td className="border p-2">{item.id}</td>
-                <td className="border p-2">{item.id}</td>
-                <td className="border p-2">{item.id}</td>
-                <td className="border p-2">{item.id}</td>
-                <td className="border p-2">{item.id}</td>
-                <td className="border p-2 text-center">
-                  <TbDatabaseEdit className='text-cyan-500 cursor-pointer'/>
-                  <TbDatabaseMinus className='text-red-600 cursor-pointer'/>  
-                </td>
-              </tr>
+        <table className="w-full border-collapse whitespace-nowrap">
+        <thead className="bg-blueLight uppercase text-sm">
+          <tr>
+            {tableHeaders.map((header) => (
+              <th key={header} className={`border p-2 ${header === "id" ? 'sticky left-0 z-[1] bg-blueLight' : ''}`}>
+                {header}
+              </th>
             ))}
-          </tbody>
+            <th className="border p-2"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentItems.map((item, index) => (
+            <tr key={item.id} className={index % 2 === 0 ? '' : 'bg-accent/10'}>
+              {tableHeaders.map((header) => (
+                <td key={header} className={`border p-2 ${header === "id" ? 'sticky left-0 z-[1]' : ''} ${index % 2 === 0 ? 'bg-[#C3C6CA]' : 'bg-[#AFB7BF]'}`}>
+                  {item[header]}
+                </td>
+              ))}
+              <td className="border p-2 text-center">
+                <TbDatabaseEdit className="text-cyan-500 cursor-pointer" />
+                <TbDatabaseMinus className="text-red-600 cursor-pointer" />
+              </td>
+            </tr>
+          ))}
+        </tbody>
         </table>
       </div>
       <div className="mt-4 flex">
