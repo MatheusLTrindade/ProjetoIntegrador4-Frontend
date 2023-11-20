9e051e6f5426557ce9e9f7d43cb455d00efec843
Matheus Lopes Trindade <matheus.lopes.trindade@hotmail.com>
2023-11-20 17:44:54 -0300

    updating Product component

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,31 +1,33 @@
+// next
 import Image from 'next/image';
+
+// react
 import React from 'react';
 
-export default function Product({ photo, name, price, photoSeller, seller }) {
+// icons
+import { MdSettings } from 'react-icons/md';
+
+// components
+import ProfilePicture from './ProfilePicture';
+
+export default function Product({ photo, name, price, amount, curCondition, photoSeller, seller, setting }) {
   return (
-    <div className="bg-primary-light p-4 rounded-md shadow-md">
-      <Image width={200} height={200} src={photo} alt={name} className="w-full h-32 object-cover mb-4 rounded-t-md" />
-      <h2 className="text-lg font-semibold text-primary-dark">{name}</h2>
-      <p className="text-primary">{seller}</p>
-      <div className="flex items-center justify-between">
-        <p className="text-accent font-bold mb-2">{price}</p>
-        <button className="bg-tertiary text-white px-4 py-2 rounded-full flex items-center">
-          Adicionar ao Carrinho
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            className="h-5 w-5 ml-2"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-            />
-          </svg>
-        </button>
+    <div className={`group flex flex-col justify-center items-center bg-white/30 p-2 rounded-md relative hover:bg-blueMedium shadow-md drop-shadow-md hover:shadow-white/50 ${setting ? 'w-[220px] h-[220px]' : ''} ${photoSeller && seller ? 'pt-8' : ''}`}>
+      {/* Header */}
+      { photoSeller && seller ? <ProfilePicture img={photoSeller} name={seller}/> : '' }
+      {/* Image */}
+      <div className={`h-[208px] w-[208px] p-1 bg-white/50 rounded-t-md shadow- shadow-black/50 drop-shadow-md ${setting ? 'transition-all ease-in duration-500 group-hover:hidden' : 'mb-2'}`} >
+        <Image width={200} height={200} src={photo} alt={name} className="w-[200px] h-[200px] object-cover rounded-t-md" />
+      </div>
+      {/* Legend */}
+      <div className={`${setting ? 'hidden w-[200px] h-[200px] transition-all ease-in duration-500 group-hover:flex flex-col justify-center items-center' : ''}`}>
+        <h2 className={`text-lg font-semibold text-primary-dark transition-all ease-in duration-300 ${setting ? '' : 'group-hover:hidden'}`}>{name}</h2>
+        <div className="flex flex-col items-center justify-between">
+          <p className="text-accent font-bold mb-2 transition-all ease-in duration-300 group-hover:text-white">{price}</p>
+          <button className={`hidden bg-tertiary text-white px-2 rounded-full items-center transition-all ease-in-out duration-300 group-hover:block hover:bg-blueExtraLight ${setting ? 'py-2' : 'py-0'}`}>
+            { setting ? <MdSettings /> : 'Solicitar troca' }
+          </button>
+        </div>
       </div>
     </div>
   );
