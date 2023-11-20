77795f959bdaf298ae2b2370b81629e49220bad7
Matheus Lopes Trindade <matheus.lopes.trindade@hotmail.com>
2023-11-20 17:12:30 -0300

    adding productPhoto api

---
+++ src/app/api/upload/productPhoto.js
@@ -0,0 +1,20 @@
+// axios
+import axios from "axios";
+
+export default async function productPhoto(formData) {
+  try {
+    // Faz a requisição POST usando Axios
+    const response = await axios.post('http://localhost:8050/product/upload', formData, { headers: {'Content-Type': 'multipart/form-data'}});
+    if (response.status === 200) {
+      console.info('Foto envida com sucesso!')
+      // Salvar o toast no armazenamento de sessão
+      sessionStorage.setItem('type', 'success')
+      sessionStorage.setItem('message', 'Foto enviada com sucesso!')
+    }
+  } catch(error) {
+    // Lida com erros da requisição aqui
+    sessionStorage.setItem('type', 'error')
+    sessionStorage.setItem('message', 'Falha ao enviar foto!')
+    console.error('Erro ao enviar requisição:', error);
+  }
+}
