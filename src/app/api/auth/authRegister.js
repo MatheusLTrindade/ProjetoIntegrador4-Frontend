769359785adeb3d270ce4ee7ed12fdb1e171a17f
Matheus Lopes Trindade <matheus.lopes.trindade@hotmail.com>
2023-11-26 07:28:05 -0300

    update authRegister api

--- src/app/api/auth/authRegister.js
+++ src/app/api/auth/authRegister.js
@@ -1,7 +1,8 @@
 // axios
 import axios from "axios";
+import userPhoto from '@/api/upload/userPhoto';
 
-export default async function authRegister(formData, router) {
+export default async function authRegister(formData, photoData, router) {
   try {
     // Faz a requisição POST usando Axios
     const response = await axios.post('http://localhost:8050/auth/register', formData)
@@ -10,8 +11,9 @@ export default async function authRegister(formData, router) {
       sessionStorage.setItem('type', 'success')
       sessionStorage.setItem('message', 'Cadastrado realizado com sucesso!')
       // Redireciona o usuário para a página de login
+      photoData.id = response.data.data.id
+			await userPhoto(photoData);
       router.push('/login')
-      return response.data.id
     }
   } catch(error) {
     // Lida com erros da requisição aqui
