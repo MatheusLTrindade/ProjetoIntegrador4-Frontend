3b742b0d044aa3a372b1a183e4592d03ec81bd5f
Matheus Lopes Trindade <matheus.lopes.trindade@hotmail.com>
2023-11-26 07:29:49 -0300

    adding getProductsPersonal api

---
+++ src/app/api/data/getProductsPersonal.js
@@ -0,0 +1,17 @@
+// axios
+import axios from "axios";
+
+export default async function getProductsPersonal() {
+  try {
+    // Faz a requisição POST usando Axios
+    const response = await axios.get('http://localhost:8050/product/findall/personal', { headers: {'Authorization': 'Bearer ' + sessionStorage.getItem('jwtToken')}});
+    if (response.status === 200) {
+      return response.data
+    }
+  } catch(error) {
+    // Lida com erros da requisição aqui
+    sessionStorage.setItem('type', 'error')
+    sessionStorage.setItem('message', 'Falha ao carregar os produtos cadastrados!')
+    console.error('Erro ao enviar requisição:', error);
+  }
+}
