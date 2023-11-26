0559c8444c116c5fab8d20d4fd906b50e1333900
Matheus Lopes Trindade <matheus.lopes.trindade@hotmail.com>
2023-11-26 07:31:06 -0300

    update next config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ const { checkTargetForNewValues } = require('framer-motion')
 
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['images.pexels.com', 'trocafacilstorage.blob.core.windows.net'],
+  },
+}
 
 module.exports = nextConfig
