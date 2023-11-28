3f0eacd53aa93725dff8e131816fa7fc7310618e
Matheus Lopes Trindade <matheus.lopes.trindade@hotmail.com>
2023-11-28 18:12:58 -0300

    adding images

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const { checkTargetForNewValues } = require('framer-motion')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.pexels.com', 'trocafacilstorage.blob.core.windows.net'],
+    domains: ['trocafacilstorage.blob.core.windows.net'],
   },
 }
 
