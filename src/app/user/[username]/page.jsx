0b0b4b7460bec6992896e92a8c661c586f751078
Matheus Lopes Trindade <matheus.lopes.trindade@hotmail.com>
2023-11-28 18:33:05 -0300

    update Profile page

--- src/app/user/[username]/page.jsx
+++ src/app/user/[username]/page.jsx
@@ -15,10 +15,12 @@ import { useState, useEffect } from 'react';
 import Modal from '@/components/Modal';
 import ProfileForm from '@/components/ProfileForm';
 import ProductUpdateForm from '@/components/ProductUpdateForm';
+import TradeForm from '@/components/TradeForm';
 import Product from '@/components/Product';
 
 // api
-import getProductsPersonal from '@/api/data/getProductsPersonal';
+import getProductsPersonal from '@/api/get/getProductsPersonal';
+import getAccountPersonal from '@/api/get/getAccountPersonal';
 
 export const metadata = {
 	title: 'Troca Fácil | Perfil',
@@ -31,92 +33,90 @@ export const metadata = {
 
 export default function Profile({ params }) {
 	const [products, setProducts] = useState([]);
-
-	async function getProducts() {
+	async function getProducts(username) {
 		try {
 			// Faz a requisição usando a função getProducts
-			const productsPersonal = await getProductsPersonal();
+			const productsPersonal = await getProductsPersonal(username);
 			const productListTemp = [];
-
-			productsPersonal.forEach((e) => {
-				productListTemp.push(e);
-			});
-
+			productsPersonal.forEach((e) => {productListTemp.push(e)});
 			setProducts(productListTemp);
-			console.log(productsPersonal);
-		} catch (error) {
-			throw error;
+		} catch (errorProduct) {
+			throw errorProduct
+		}
+	}
+	
+	const [account, setAccount] = useState([]);
+	async function getAccount(username) {
+		try {
+			// Faz a requisição usando a função getAccount
+			const accountPersonal = await getAccountPersonal(username);
+			setAccount(accountPersonal);
+		} catch (errorAccount) {
+			throw errorAccount
 		}
 	}
 
 	const pathname = usePathname();
-
-	// Verificar sessionStorage a cada 2 segundos
+	// Verificar alteração de pathname
 	useEffect(() => {
-		// Verificar se pathname começa com '/'
-		if (pathname && pathname.startsWith('/user/')) {
-			getProducts();
+		if (params.username) {
+			getAccount(params.username);
+			getProducts(params.username);
 		}
-	}, []);
+	}, [params.username]);
 
 	const [modalPersonalView, setModalPersonalView] = useState(false);
-	const [modalProductView, setModalProductView] = useState(false);
-	const [productChange, setProductChange] = useState([]);
 	function handleModalPersonalChange() {
 		setModalPersonalView(true);
 	}
 	function handleModalPersonalClose() {
 		setModalPersonalView(false);
 	}
+	const [modalProductView, setModalProductView] = useState(false);
+	const [productChange, setProductChange] = useState([]);
 	function handleModalProductChange(product) {
 		setProductChange(product);
-		console.log(productChange)
+		console.log(productChange);
 		setModalProductView(true);
 	}
 	function handleModalProductClose() {
 		setModalProductView(false);
 	}
+	const [modalTradeView, setModalTradeView] = useState(false);
+  const [productSelected, setProductSelected] = useState()
+	function handleModalTradeChange(produto) {
+    setProductSelected(produto)
+    setModalTradeView(true);
+	}
+  function handleModalTradeClose() {
+    setModalTradeView(false);
+  }
 
 	return (
 		<div className='h-full bg-secondary/20 text-center relative'>
-			<Modal
-				visible={modalPersonalView}
-				title={'Dados pessoais'}
-				component={ProfileForm}
-				onClose={handleModalPersonalClose}
-			/>
-			<Modal
-				visible={modalProductView}
-				title={'Alterar Produto'}
-				component={() => <ProductUpdateForm product={productChange} />}
-				onClose={handleModalProductClose}
-			/>
+			<Modal visible={modalPersonalView}	title={'Dados pessoais'} component={() => <ProfileForm onClose={handleModalPersonalClose} />} onClose={handleModalPersonalClose}/>
+			<Modal visible={modalProductView} title={'Alterar Produto'} component={() => <ProductUpdateForm product={productChange} onClose={handleModalProductClose} />} onClose={handleModalProductClose}/>
+			<Modal visible={modalTradeView} title={'Criar proposta'} component={() => <TradeForm product={productSelected} onClose={handleModalTradeClose} />} onClose={handleModalTradeClose}/>
 			<div className='container mx-auto h-full flex flex-col justify-center sm:pt-20'>
 				<div className='flex flex-col sm:flex-row justify-center items-center relative mb-0 sm:mb-4'>
 					<div className='hidden sm:flex flex-1 justify-center before:h-[2px] xl:before:bg-white/50 xl:before:absolute xl:before:left-0 xl:before:w-full relative xl:pl-20 max-sm:-mt-6'></div>
 					<div className='flex w-[150px] h-[150px] sm:w-[200px] sm:h-[200px]'>
 						<div className='flex justify-center items-center p-1 rounded-full bg-white/50 w-[150px] h-[150px] sm:w-[200px] sm:h-[200px]'>
-							<Image
-								width={190}
-								height={190}
-								src={
-									'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg'
-								}
-								alt='Profile Picture'
-								className='rounded-full w-[140px] h-[140px] sm:w-[190px] sm:h-[190px] object-cover'
-							/>
+							<Image width={190} height={190}	src={account.photoPath !== 'no image' ? account.photoPath : '/PersonNoImage.png'} alt='Profile Picture' className='rounded-full w-[140px] h-[140px] sm:w-[190px] sm:h-[190px] object-cover' />
 						</div>
-						<div className='absolute ml-32 sm:ml-44 flex justify-center items-center gap-2 z-10'>
-							<MdSettings
-								className='text-blueExtraLight cursor-pointer hover:text-blueLight sm:text-xl'
-								onClick={handleModalPersonalChange}
-							/>
-							<Link
-								href={`/user/${params.username}/notifications`}
-								className='text-blueExtraLight cursor-pointer hover:text-blueLight sm:text-xl'>
-								<MdNotifications />
-							</Link>
-						</div>
+						{pathname.endsWith(typeof window !== 'undefined' ? `/user/${sessionStorage.getItem('username')}` : null) ? 
+							<div className='absolute ml-32 sm:ml-44 flex justify-center items-center gap-2 z-10'>
+								<MdSettings
+									className='text-blueExtraLight cursor-pointer hover:text-blueLight sm:text-xl'
+									onClick={handleModalPersonalChange}
+								/>
+								<Link
+									href={`/user/${params.username}/notifications`}
+									className='text-blueExtraLight cursor-pointer hover:text-blueLight sm:text-xl'>
+									<MdNotifications />
+								</Link>
+							</div>
+						: ''}
 					</div>
 					<h1 className='h1 sm:absolute sm:mb-20 sm:left-0 nova-slim'>
 						<span className='text-accent'>@</span>
@@ -129,11 +129,14 @@ export default function Profile({ params }) {
 							<Product
 								key={index}
 								id={product.id}
-								photo={product.photoPath == 'no image' ? '/DevMurilo.jpeg' : product.photoPath}
+								photo={product.photoPath == 'no image'	? '/ProductNoImage.png' : product.photoPath}
 								name={product.name}
 								price={product.price}
+								amount={product.amount}
+								curCondition={product.curCondition}
 								setting={true}
-								click={() => {handleModalProductChange(product)}}
+								admin={pathname.endsWith(typeof window !== 'undefined' ? `/user/${sessionStorage.getItem('username')}` : null) ? true : false}
+								click={() => {pathname.endsWith(typeof window !== 'undefined' ? `/user/${sessionStorage.getItem('username')}` : null) ? handleModalProductChange(product) : handleModalTradeChange(product)}}
 							/>
 						))}
 					</div>
